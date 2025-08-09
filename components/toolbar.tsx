7b2e13e55aa7548dbad7764edd537ed3b8bdade8
zsiza <zsemberiizabella@gmail.com>
2025-08-09 11:34:49 +0200

    documents, toolbar edit

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -2,14 +2,49 @@
 import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./icon-picker";
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
+import { ImageIcon, Smile, X } from "lucide-react";
 import { init } from "next/dist/compiled/webpack/webpack";
+import { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import TextareaAutosize from "react-textarea-autosize";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
   preview?: boolean;
 }
 export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEdititng] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEdititng(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => setIsEdititng(false);
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
   return (
     <div className="pl-[54p] group relative">
       {!!initialData.icon && !preview && (
@@ -31,6 +66,48 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
       )}
       {!!initialData.icon && preview && (
         <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+      <div className="opacity-0 group-hover:opacity-100 felx items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => {}}>
+            <Button
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="icon"
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              Add Icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Add cover
+          </Button>
+        )}
+      </div>
+      {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className=" text-5xl bg-transparent font-bold break-words outline-none text-[#3f3f3f] dark:text-[#cfcfcf] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3f3f3f] dark:text-[#cfcfcf] "
+        >
+          {initialData.title}
+        </div>
       )}
     </div>
   );
