73ae1818b4d2ac678e4bb5ab56687582863c7611
zsiza <zsemberiizabella@gmail.com>
2025-08-09 10:43:58 +0200

    banner, navbar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -11,7 +11,7 @@ import {
   Trash,
   X,
 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, use, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
@@ -28,10 +28,12 @@ import {
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const settings = useSettings();
   const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -181,15 +183,19 @@ export const Navigation = () => {
           isMobile && "w-full left-0"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
---
+++ components/toolbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+import { init } from "next/dist/compiled/webpack/webpack";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  return (
+    <div className="pl-[54p] group relative">
+      {!!initialData.icon && !preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={() => {}}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opcity-100 transition text-muted-foreground text-xs"
+            variant="outline"
+            size="icon"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </div>
+      )}
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+    </div>
+  );
+};
